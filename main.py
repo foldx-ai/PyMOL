8c6ab497b10cb82580c101e10f981598f83a33b6
Hanson Jiang <hanson.b.jiang@gmail.com>
2023-05-16 18:57:03 -0400

    Add main

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import pymol
+import requests
+
+URI = "https://api.esmatlas.com/foldSequence/v1/pdb/"
+
+def foldx(sequence, name=None):
+    '''Given a protein sequence foldx will generate a pdb file of string and load it into pymol.'''
+    response =requests.post(URI, data=sequence)
+    pymol.cmd.read_pdbstr(response.text, name)
+
+pymol.cmd.extend('foldx', foldx)
