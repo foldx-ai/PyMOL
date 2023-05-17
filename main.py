20d39c672080757930ddbfd8d5e1486d10de1778
Horace Jiang <horace.z.jiang@gmail.com>
2023-05-16 20:31:29 -0400

    make long lines shorter

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import requests
 URI = "https://api.esmatlas.com/foldSequence/v1/pdb/"
 
 def foldx(sequence, name=None):
-    '''Given a protein sequence foldx will generate a pdb file of string and load it into pymol.'''
+    '''Given a protein sequence foldx will generate a pdb 
+    file of string and load it into pymol.'''
     response =requests.post(URI, data=sequence)
     pymol.cmd.read_pdbstr(response.text, name)
 
