07d4812bbadd907f18a6b20bacb7964f410ae2c7
Hanson Jiang <hanson.b.jiang@gmail.com>
2023-05-24 19:42:51 -0400

    automatic name generation

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pymol
 import requests
+import hashlib
 
 URI = "https://api.esmatlas.com/foldSequence/v1/pdb/"
 
 def foldx(sequence:str, name: str=None) -> None:
     '''Given a protein sequence foldx will generate a pdb 
     file of string and load it into pymol.'''
+    if not name:
+        name = hashlib.md5(sequence.encode('utf-8')).hexdigest()[:6]
     sequence = sequence.upper()
     response =requests.post(URI, data=sequence)
     if response.status_code > 299:
