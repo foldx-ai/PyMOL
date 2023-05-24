4a549cfa9b0497da689fdc2be76d755b2b81aa92
Hanson Jiang <hanson.b.jiang@gmail.com>
2023-05-24 19:22:56 -0400

    This will make the sequence uppercase.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ URI = "https://api.esmatlas.com/foldSequence/v1/pdb/"
 def foldx(sequence, name=None):
     '''Given a protein sequence foldx will generate a pdb 
     file of string and load it into pymol.'''
+    sequence = sequence.upper()
     response =requests.post(URI, data=sequence)
     pymol.cmd.read_pdbstr(response.text, name)
 
