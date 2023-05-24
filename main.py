4fb5d4b7373226155ca815412b9d53240809cdaf
Hanson Jiang <hanson.b.jiang@gmail.com>
2023-05-24 19:32:38 -0400

    Check response code

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import requests
 
 URI = "https://api.esmatlas.com/foldSequence/v1/pdb/"
 
-def foldx(sequence, name=None):
+def foldx(sequence:str, name: str=None) -> None:
     '''Given a protein sequence foldx will generate a pdb 
     file of string and load it into pymol.'''
     sequence = sequence.upper()
     response =requests.post(URI, data=sequence)
+    if response.status_code > 299:
+        print('error:' + response.text)
+        return
     pymol.cmd.read_pdbstr(response.text, name)
 
 pymol.cmd.extend('foldx', foldx)
